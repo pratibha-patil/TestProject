51db2fd2e3f9eb42eae49b6c88afd61b44fe7c8e
Pratibha Patil <pratibha.patol.2022@gmail.com>
2022-05-17 21:41:48 +0530

    added single line on new branch

--- src/main/java/com/model/Student.java
+++ src/main/java/com/model/Student.java
@@ -33,6 +33,7 @@ public class Student {
 
     public static void main(String args[])
     {
+        System.out.println("My Details:");
         Student s=new Student("abc","xyz");
         s.Display();
     }
